0a37982281d6bfafd2eb0295ea326872b10b8ade
Noble Arulraj <104187141+Noblearul21@users.noreply.github.com>
2023-01-31 14:01:14 +0530

    index

---
+++ forms/contact.php
@@ -0,0 +1,11 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+$to =" From: $name \n Message: $message";
+$recipient = "noblearul806@gmail.com";
+$from = "From: $email \r\n";
+mail($recipient, $to, $from);
+echo "Thank You!";
+?>
